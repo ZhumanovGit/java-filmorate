edd0ac36b2926d705cace7b5bca5782ab86f6227
Zhumanov Timur <zhumanov.timur2015@yandex.ru>
2023-09-02 16:39:13 +0300

    code-style: внести правки в внешний вид кода

--- src/main/java/ru/yandex/practicum/filmorate/exception/ValidateException.java
+++ src/main/java/ru/yandex/practicum/filmorate/exception/ValidateException.java
@@ -1,6 +1,8 @@
 package ru.yandex.practicum.filmorate.exception;
 
-public class ValidateException extends RuntimeException{
+public class ValidateException extends RuntimeException {
 
-    public ValidateException(String message) {super(message);}
+    public ValidateException(String message) {
+        super(message);
+    }
 }
