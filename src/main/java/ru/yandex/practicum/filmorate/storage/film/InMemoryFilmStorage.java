dc28d0c10c2a911bdb1e74e52f628e66c55265ba
Zhumanov Timur <zhumanov.timur2015@yandex.ru>
2023-09-11 19:47:51 +0300

    feature: создать реализацию InMemoryFilmStorage для FilmStorage

---
+++ src/main/java/ru/yandex/practicum/filmorate/storage/film/InMemoryFilmStorage.java
@@ -0,0 +1,81 @@
+package ru.yandex.practicum.filmorate.storage.film;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import ru.yandex.practicum.filmorate.service.ValidateService;
+import ru.yandex.practicum.filmorate.exception.NotFoundException;
+import ru.yandex.practicum.filmorate.model.Film;
+
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+@Component
+public class InMemoryFilmStorage implements FilmStorage{
+
+    final Map<Integer, Film> films;
+    int id;
+    ValidateService validateService;
+
+    @Autowired
+    public InMemoryFilmStorage(ValidateService validateService) {
+        this.validateService = validateService;
+        this.films = new LinkedHashMap<>();
+        this.id = 0;
+    }
+
+    int createId() {
+
+        return ++id;
+    }
+    @Override
+    public Film createFilm(Film film) {
+        validateService.validateCreateFilm(film);
+
+        film.setId(createId());
+
+        films.put(film.getId(), film);
+        return film;
+    }
+
+    @Override
+    public Film updateFilm(Film film) {
+        validateService.validateUpdateFilm(film);
+
+        if (films.get(film.getId()) == null) {
+            throw new NotFoundException("Такого фильма еще не существует в библиотеке");
+        }
+
+        films.put(film.getId(), film);
+        return film;
+    }
+
+    @Override
+    public void deleteFilm(int id) {
+        if (films.remove(id) == null) {
+            throw new NotFoundException("Такого фильма еще не существует в библиотеке");
+        }
+
+    }
+
+    @Override
+    public void deleteAllFilms() {
+        films.clear();
+    }
+
+    @Override
+    public List<Film> getFilms() {
+        return new ArrayList<>(films.values());
+    }
+
+    @Override
+    public Film getFilmById(int id) {
+        Film film = films.get(id);
+        if (film == null) {
+            throw new NotFoundException("Такого фильма еще не существует в библиотеке");
+        }
+
+        return film;
+    }
+}
