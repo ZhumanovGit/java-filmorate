9c6b00c1624c790e37a491280d3a04e25a7cd92f
Zhumanov Timur <zhumanov.timur2015@yandex.ru>
2023-09-11 19:52:57 +0300

    feature: добавить поле для хранения лайков

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -4,6 +4,7 @@ import lombok.Builder;
 import lombok.Data;
 
 import java.time.LocalDate;
+import java.util.Set;
 
 @Data
 @Builder
@@ -13,6 +14,7 @@ public class Film {
     private String description;
     private LocalDate releaseDate;
     private int duration;
+    private Set<Long> likedUsers;
 
 
 }
