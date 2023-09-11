bbd41d2c84b2dd100071b5dc05324e2499099c2d
Zhumanov Timur <zhumanov.timur2015@yandex.ru>
2023-09-11 19:44:00 +0300

    refactor: перенести ValidateService в пакет service

--- src/test/java/ru/yandex/practicum/filmorate/ValidateServiceTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/ValidateServiceTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 import ru.yandex.practicum.filmorate.exception.ValidateException;
 import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.model.User;
+import ru.yandex.practicum.filmorate.service.ValidateService;
 
 import java.time.LocalDate;
 import java.time.Month;
