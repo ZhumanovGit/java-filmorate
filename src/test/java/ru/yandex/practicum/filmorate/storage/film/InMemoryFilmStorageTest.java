c6917e9a754a5d05cf03caa19add4bb496516e32
Zhumanov Timur <zhumanov.timur2015@yandex.ru>
2023-09-15 17:13:02 +0300

    code-style: исправить мелкие недочеты

--- src/test/java/ru/yandex/practicum/filmorate/storage/film/InMemoryFilmStorageTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/storage/film/InMemoryFilmStorageTest.java
@@ -2,9 +2,9 @@ package ru.yandex.practicum.filmorate.storage.film;
 
 import ru.yandex.practicum.filmorate.service.ValidateService;
 
-class InMemoryFilmStorageTest extends FilmStorageTest{
+class InMemoryFilmStorageTest extends FilmStorageTest {
     @Override
-    public FilmStorage getStorage(){
+    public FilmStorage getStorage() {
         return new InMemoryFilmStorage(new ValidateService());
     }
 }
--- src/test/java/ru/yandex/practicum/filmorate/storage/user/InMemoryUserStorageTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/storage/user/InMemoryUserStorageTest.java
@@ -2,7 +2,7 @@ package ru.yandex.practicum.filmorate.storage.user;
 
 import ru.yandex.practicum.filmorate.service.ValidateService;
 
-public class InMemoryUserStorageTest extends UserStorageTest{
+public class InMemoryUserStorageTest extends UserStorageTest {
 
     @Override
     public UserStorage getStorage() {
