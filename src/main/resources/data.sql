f27cd9e9c2eda0330acc1e5814dd88b93c7fe31c
Zhumanov Timur <zhumanov.timur2015@yandex.ru>
2023-10-14 17:18:08 +0300

    code-style: убрать лишние импорты

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,6 @@
+DELETE FROM genre;
+DELETE FROM ratingMPA;
+
 INSERT INTO genre (name) VALUES ('Комедия');
 INSERT INTO genre (name) VALUES ('Драма');
 INSERT INTO genre (name) VALUES ('Мультфильм');
